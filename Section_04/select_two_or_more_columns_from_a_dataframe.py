1706e678a64ef9c913aa136dde75fbdc23739d13
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2022-06-28 18:37:04 +0200

    Done with 77

--- Section_04/select_two_or_more_columns_from_a_dataframe.py
+++ Section_04/select_two_or_more_columns_from_a_dataframe.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import secret as se
 
-print(se.SECREAT)
-
 # If you see a test failure when checking your solution,
 # note that [left] refers to YOUR code while [right]
 # refers to the correct code that the computer is comparing
@@ -13,11 +11,24 @@ print(se.SECREAT)
 # Name, Original Location, Year, Headquarters, Locations, Areas Served
 # Import the CSV into a DataFrame and assign it to a "chicken" variable.
 
+chicken = pd.read_csv(se.SECREAT + "chicken_restaurants.csv")
+
 
 # Extract the "Year" and "Locations" columns (in that order) into
 # their own DataFrame. Assign the DataFrame to a "years_and_locations" variable.
 
+locations = ['Year', 'Locations']
+
+years_and_locations = chicken[locations]
 
 # Extract the "Locations", "Name", and "Headquarters" columns (in that order)
 # into their own DataFrame. Assign the DataFrame to a
 # "interesting_facts" variable.
+
+locations = ['Locations', 'Name', 'Headquarters']
+
+interesting_facts = chicken[locations]
+
+# print(chicken)
+# print(years_and_locations)
+print(interesting_facts)
